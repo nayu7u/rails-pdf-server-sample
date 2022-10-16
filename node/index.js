77a2b92eb50175ab56bc728bad3cfc8b302f74a1
nayu7u <yusuke.nadaka@gmail.com>
2022-10-17 00:37:13 +0900

    feat: save pdf by playwright

--- node/index.js
+++ node/index.js
@@ -1,8 +1,18 @@
-import playwright from "playwright"
+const playwright = require("playwright")
+const express = require("express")
+const app = express()
+const port = 3001
+const crypto = require("crypto")
 
-const browser = await playwright.chromium.launch({ headless: true })
-const context = await browser.newContext()
-const page = await context.newPage()
-const url = "https://playwright.dev/docs/api/class-page#page-pdf"
-await page.goto(url, { waitUntil: "networkidle"})
-await page.pdf({ format: "A4", path: "output/test.pdf"})
+app.get("/", async (req, res, next) => {
+  const browser = await playwright.chromium.launch({ headless: true })
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  const url = "https://playwright.dev/docs/api/class-page#page-pdf"
+  await page.goto(url, { waitUntil: "networkidle" })
+  const path = `output/${crypto.randomUUID()}.pdf`
+  await page.pdf({ format: "A4", path: path })
+  res.status(200).json({ path: path})
+})
+
+app.listen(port)
